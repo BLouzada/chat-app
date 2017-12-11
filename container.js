06cfa76ea96d3220c5846eaf1dd4eceb7df9ea1e
Bruno <Bruno@ubots.com.br>
2017-12-11 08:31:32 -0200

    adicionando contianer dependable e standart js

--- container.js
+++ container.js
@@ -1,8 +1,19 @@
-const dependable = require('dependable');
-const path = require('path');
+const dependable = require('dependable')
+const path = require('path')
 
-const container = dependable.container();
-const simpleDependencies = [ 
-    ['_', 'lodash'],
-    []
+const container = dependable.container()
+const simpleDependencies = [
+  ['_', 'lodash'],
+  []
 ]
+simpleDependencies.forEach((dependecie) => {
+  container.register(dependecie[0], () => {
+    return require(dependecie[1])
+  })
+})
+container.load(path.join(__dirname, '/controllers'))
+container.load(path.join(__dirname, '/helpers'))
+container.register('container', () => {
+  return container
+})
+module.exports = container
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const http = require('http')
+const container = require('./container')
+
+container.resolve(() => {
+  const setupExpress = () => {
+    const app = express()
+    const server = http.createServer(app)
+    server.listen(3000, () => {
+      console.log('listening on 3000')
+    })
+  }
+  const app = setupExpress()
+})
